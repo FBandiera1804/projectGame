91797a5e1bbd3a5872a6004017ebf768840f2bde
AgoDiMarzo <gstndimarzo410@gmail.com>
2022-12-16 12:16:17 +0100

    Scritta una storia adeguata per lo stage 1, la struttura del gioco base è finita.

--- src/main/java/com/projectGame/App.java
+++ src/main/java/com/projectGame/App.java
@@ -53,11 +53,13 @@ public class App {
         st1.setSceltaTre(3);
         st1.setSceltaQuattro(4);
         
-        System.out.println("Ti sei risvegliato in una foresta buia.\n In lontananza trovi un drago ostile molto pericoloso. \n");
-        System.out.println("Cosa scegli di fare?");
-        System.out.println("1) Combatti il drago a mani nude, tanto cosa potrà mai andare storto?");
+        System.out.println("Ti sei risvegliato in una foresta buia. Hai perso i sensi e sei disorientato in piena notte."
+        		+ "\nVicino trovi un trovi un drago ostile coperto da alberi. Non hai alcun mezzo per combatterlo e ti senti impaurito.\n"
+        		+ "Cosa scegli di fare per sopravvivere? (Fai la tua scelta digitando il numero di quest'ultima.)\n");
+        
+        System.out.println("1) Prendi il coraggio e provi ad attaccarlo a mani nude.");
         System.out.println("2) Scappi attraverso la foresta sperando di non essere visto");
-        System.out.println("3) Stringo ");
+        System.out.println("3) Ti nascondi dietro un albero fino a quando il drago non se ne va");
         System.out.println("4) Ritirati");
         
         st1.setSceltaSelezionata(userInput.nextInt());
@@ -65,16 +67,16 @@ public class App {
         switch(st1.getSceltaSelezionata()) {
         	case 1:
         		st1.setSceltaSelezionata(1);
-        		System.out.println("Mi dispiace, sei stato cotto a puntino dal drago.");
+        		System.out.println("Hai perso. Sei stato bruciato vivo con un soffio.");
         		System.exit(0);
         	break;
         	case 2:
         		st1.setSceltaSelezionata(2);
-        		System.out.println("Sei riuscito a scappare! Hai superato il livello. \n");
+        		System.out.println("\nSei riuscito a scappare! Hai superato il livello. \n");
         	break;
         	case 3:
         		st1.setSceltaSelezionata(3);
-        		System.out.println("Hai riavviato il gioco.");
+        		System.out.println("Hai perso. Il drago ti ha trovato, e ti attacca dallo spavento. Vieni mangiato vivo.");
         		System.exit(0);
         	break;
         	case 4:
@@ -85,7 +87,7 @@ public class App {
         }
         
         //Scelta continua gioco, riavvio o fine gioco.
-        System.out.println("Vuoi continuare?");
+        System.out.println("Vuoi continuare? \n");
         System.out.println("1) Passa al prossimo livello.");
         System.out.println("2) Riavvia il gioco.");
         System.out.println("3) Ritirati.");
@@ -125,7 +127,7 @@ public class App {
         switch(st2.getSceltaSelezionata()) {
         case 1:
         	st2.setSceltaSelezionata(1);
-        	System.out.println("Sei a costruire una lancia e a procurarti del cibo.");
+        	System.out.println("Sei a costruire una lancia e a procurarti del cibo. \n Hai vinto.\n ");
         break;
         case 2:
         	st2.setSceltaSelezionata(2);
@@ -142,6 +144,25 @@ public class App {
         	System.exit(0);
         }
         
+        //Scelta continua gioco, riavvio o fine gioco.
+        System.out.println("Vuoi continuare?");
+        System.out.println("1) Passa al prossimo livello.");
+        System.out.println("2) Riavvia il gioco.");
+        System.out.println("3) Ritirati.");
+        
+        st2.setSceltaSelezionata(userInput.nextInt());
+        
+        switch(st2.getSceltaSelezionata()) {
+        case 1:
+        	System.out.println();
+        break;
+        case 2:
+        	System.exit(0);
+        break;
+        case 3:
+        	System.exit(0);
+        break;
+        }
         clearConsole();
         
         //Stage 3
@@ -162,7 +183,7 @@ public class App {
         switch(st3.getSceltaSelezionata()) {
         case 1:
         	st3.setSceltaUno(1);
-        	System.out.println("Hai accettato l'invito e la strega ti guida verso la sua casa");
+        	System.out.println("Hai accettato l'invito e la strega ti guida verso la sua casa. \n Hai superato il  livello.\n ");
         break;
         case 2:
         	st3.setSceltaDue(2);
@@ -181,35 +202,76 @@ public class App {
         break;
         }
         
-        clearConsole();
-        
-        //Stage 4 
-        System.out.println("e");
-        System.out.println("1) e");
-        System.out.println("2) e");
-        System.out.println("3) Ritirati");
-        System.out.println("4) Riavvia gioco");
+        //Scelta continua gioco, riavvio o fine gioco.
+        System.out.println("Vuoi continuare?");
+        System.out.println("1) Passa al prossimo livello.");
+        System.out.println("2) Riavvia il gioco.");
+        System.out.println("3) Ritirati.");
         
         st3.setSceltaSelezionata(userInput.nextInt());
         
         switch(st3.getSceltaSelezionata()) {
         case 1:
-        	st3.setSceltaUno(1);
-        	System.out.println("w");
-        	System.exit(0);
+        	System.out.println();
         break;
         case 2:
-        	st3.setSceltaDue(2);
-        	System.out.println("w");
+        	System.exit(0);
         break;
         case 3:
-        	st3.setSceltaTre(3);
-        	System.out.println("Ti sei ritirato, hai perso.");
+        	System.exit(0);
+        break;
+        }
+        
+        clearConsole();
+        
+        //Stage 4
+        Stage4 st4 = new Stage4();
+        System.out.println("Sei arrivato alla casa della strega. Appena entrati dentro, la casa si trasforma in un'arena e vieni sfidato dalla strega.\n Accetti?");
+        System.out.println("1) Si, accetti");
+        System.out.println("2) No, rifiuti");
+        System.out.println("3) Ritirati");
+        System.out.println("4) Riavvia gioco");
+        
+        st4.setSceltaSelezionata(userInput.nextInt());
+        
+        switch(st4.getSceltaSelezionata()) {
+        case 1:
+        	st4.setSceltaUno(1);
+        	System.out.println("Durante lo scontro riesci a rompere lo scettro della strega usando la lancia e la sconfiggi.\nHai superato il livello");
+        break;
+        case 2:
+        	st4.setSceltaDue(2);
+        	System.out.println("La strega, sentendosi presa in giro, ti coglie di sopresa mentre cerchi di uscire dalla casa e muori.\nHai perso");
+        	System.exit(0);
+        break;
+        case 3:
+        	st4.setSceltaTre(3);
+        	System.out.println("Ti sei ritirato, che peccato.");
         	System.exit(0);
         break;
         case 4: 
-        	st3.setSceltaQuattro(4);
+        	st4.setSceltaQuattro(4);
         	System.out.println("Hai riavviato il gioco");
+        	System.exit(0);
+        break;
+        }
+        
+        //Scelta continua gioco, riavvio o fine gioco.
+        System.out.println("Vuoi continuare?");
+        System.out.println("1) Passa al prossimo livello.");
+        System.out.println("2) Riavvia il gioco.");
+        System.out.println("3) Ritirati.");
+        
+        st4.setSceltaSelezionata(userInput.nextInt());
+        
+        switch(st4.getSceltaSelezionata()) {
+        case 1:
+        	System.out.println();
+        break;
+        case 2:
+        	System.exit(0);
+        break;
+        case 3:
         	System.exit(0);
         break;
         }
@@ -217,31 +279,32 @@ public class App {
         clearConsole();
         
         //Stage 5
-        System.out.println("w");
-        System.out.println("1) w");
-        System.out.println("2) w");
+        Stage5 st5 = new Stage5();
+        System.out.println("La strega ormai non può più contrattaccare. \n Cosa vuoi fare?");
+        System.out.println("1) Finiscila");
+        System.out.println("2) Risparmiala");
         System.out.println("3) Ritirati");
         System.out.println("4) Riavvia gioco");
         
-        st3.setSceltaSelezionata(userInput.nextInt());
+        st5.setSceltaSelezionata(userInput.nextInt());
         
-        switch(st3.getSceltaSelezionata()) {
+        switch(st5.getSceltaSelezionata()) {
         case 1:
-        	st3.setSceltaUno(1);
-        	System.out.println("w");
+        	st5.setSceltaUno(1);
+        	System.out.println("Hai ucciso la strega, ma rimarrai chiuso in quella casa fino alla morte. \n Hai perso");
         break;
         case 2:
-        	st3.setSceltaDue(2);
-        	System.out.println("w");
+        	st5.setSceltaDue(2);
+        	System.out.println("Lasci in vita la strega, lei si scusa e ti riporta a casa. \n Hai vinto!");
         	System.exit(0);
         break;
         case 3:
-        	st3.setSceltaTre(3);
+        	st5.setSceltaTre(3);
         	System.out.println("Ti sei ritirato, hai perso.");
         	System.exit(0);
         break;
         case 4: 
-        	st3.setSceltaQuattro(4);
+        	st5.setSceltaQuattro(4);
         	System.out.println("Hai riavviato il gioco");
         	System.exit(0);
         break;
